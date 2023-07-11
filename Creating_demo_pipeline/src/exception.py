e36666a0f4815f463864f6360a932fb8af0d2463
Yipu Lerina <yipulerina29@gmail.com>
2023-07-11 11:23:14 +0530

    commit

--- Creating_demo_pipeline/src/exception.py
+++ Creating_demo_pipeline/src/exception.py
@@ -21,10 +21,8 @@ class CustomException(Exception):
         return self.error_message    
     
 '''
-
 if __name__=="__main__":
     logging.info("Logging has started")
-
     try:
         a=1/0
     except Exception as e:
